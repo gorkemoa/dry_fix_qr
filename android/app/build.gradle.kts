ac830a5d458e7556897f3d7daaa2b8e36688d19a
gorkemoa <gorkemoa35@gmail.com>
2026-02-03 16:49:50 +0300

    qr scann

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.smartmetrics.dryfix_qr"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 34
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
