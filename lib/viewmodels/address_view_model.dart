39b016ed641d03ecb5af2462a22631fd83db40e2
gorkemoa <gorkemoa35@gmail.com>
2026-02-05 12:16:56 +0300

    feat: Implement city and district selection functionality for address forms.

--- lib/viewmodels/address_view_model.dart
+++ lib/viewmodels/address_view_model.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/foundation.dart';
 import '../core/network/api_result.dart';
 import '../models/address_model.dart';
+import '../models/city_model.dart';
+import '../models/district_model.dart';
 import '../services/address_service.dart';
 import '../core/utils/logger.dart';
 
@@ -12,10 +14,18 @@ class AddressViewModel extends ChangeNotifier {
   bool _isLoading = false;
   String? _errorMessage;
   List<Address> _addresses = [];
+  List<City> _cities = [];
+  List<District> _districts = [];
+  City? _selectedCity;
+  District? _selectedDistrict;
 
   bool get isLoading => _isLoading;
   String? get errorMessage => _errorMessage;
   List<Address> get addresses => _addresses;
+  List<City> get cities => _cities;
+  List<District> get districts => _districts;
+  City? get selectedCity => _selectedCity;
+  District? get selectedDistrict => _selectedDistrict;
 
   Future<void> fetchAddresses() async {
     _isLoading = true;
@@ -114,5 +124,61 @@ class AddressViewModel extends ChangeNotifier {
 
   void refresh() {
     fetchAddresses();
+  }
+
+  Future<void> fetchCities() async {
+    _isLoading = true;
+    _errorMessage = null;
+    notifyListeners();
+
+    final result = await _addressService.getCities();
+
+    if (result is Success<List<City>>) {
+      _cities = result.data;
+    } else if (result is Failure<List<City>>) {
+      _errorMessage = result.errorMessage;
+    }
+
+    _isLoading = false;
+    notifyListeners();
+  }
+
+  Future<void> fetchDistricts(int cityId) async {
+    _isLoading = true;
+    _errorMessage = null;
+    notifyListeners();
+
+    final result = await _addressService.getDistricts(cityId);
+
+    if (result is Success<List<District>>) {
+      _districts = result.data;
+    } else if (result is Failure<List<District>>) {
+      _errorMessage = result.errorMessage;
+    }
+
+    _isLoading = false;
+    notifyListeners();
+  }
+
+  void setSelectedCity(City? city) {
+    _selectedCity = city;
+    _selectedDistrict = null;
+    _districts = [];
+    if (city != null) {
+      fetchDistricts(city.id);
+    }
+    notifyListeners();
+  }
+
+  void setSelectedDistrict(District? district) {
+    _selectedDistrict = district;
+    notifyListeners();
+  }
+
+  void resetSelection() {
+    _selectedCity = null;
+    _selectedDistrict = null;
+    _districts = [];
+    notifyListeners();
   }
 }
